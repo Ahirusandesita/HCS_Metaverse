d56bd330dabbc45d27274cc0933879fcc10ed648
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2024-05-15 10:16:48 +0900

    編集 >> アラカルト

--- Assets/Scripts/GeneralPurpose/ObjectPooler.cs
+++ Assets/Scripts/GeneralPurpose/ObjectPooler.cs
@@ -289,7 +289,7 @@ public class ObjectPooler<T> : IReturnablePool, IGettablePool, IDisposable
     }
 }
 
-public class EffectPooler
+public class ParticlePooler
 {
     private ParticleSystem particle = default;
     private GameObject parent = default;
@@ -303,7 +303,7 @@ public class EffectPooler
     /// <br>- newした段階でInstantiateが走るため注意</br>
     /// <br>- newの戻り値はIGettablePoolインターフェースで受け取ること</br>
     /// </summary>
-    public EffectPooler(PoolParticleAsset createEfectData, string parentName = null, Transform setParent = null)
+    public ParticlePooler(PoolParticleAsset createEfectData, string parentName = null, Transform setParent = null)
     {
         particle = createEfectData.Partilce;
         objectPool = new();
