b98e472dd72b8f2f4a252e1e3ac5b5fda3b0d59a
STD\s20227005 <Ahirusandesita>
2024-05-20 14:40:00 +0900

    Chat機能　オンライン

--- Assets/Scripts/FlickInputs/SendChat.cs
+++ Assets/Scripts/FlickInputs/SendChat.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 public class SendChat : MonoBehaviour
 {
     private PhotonView _PhotonViewControl;
-    
+
+    private ChatSystem chatSystem;
     private void Awake()
     {
 
         _PhotonViewControl = GetComponent<PhotonView>();
+
+        chatSystem = GameObject.FindObjectOfType<ChatSystem>();
 
     }
 
@@ -16,12 +19,12 @@ public class SendChat : MonoBehaviour
     {
 
         _PhotonViewControl.RPC("Message", RpcTarget.Others, message);
-
+        chatSystem.SendManually(message);
     }
 
     [PunRPC]
     private void Message(string message)
     {
-
+        chatSystem.SendManually(message);
     }
 }
