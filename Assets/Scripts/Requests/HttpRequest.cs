6e8eea9b04ce9f10c8fdd08b1b614dddcf1ded67
STD\s20227005 <Ahirusandesita>
2024-06-05 11:59:25 +0900

    HttpRequest テスト

--- Assets/Scripts/Requests/HttpRequest.cs
+++ Assets/Scripts/Requests/HttpRequest.cs
@@ -25,6 +25,15 @@ public class HttpRequest : MonoBehaviour
         }
         else if (req.responseCode == 200)
         {
+            Data data = new Data("MetaMan", 20);
+            byte[] postData = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(data));
+            var request = new UnityWebRequest(url, "POST");
+            request.uploadHandler = (UploadHandler)new UploadHandlerRaw(postData);
+            request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+            yield return request.Send();
+
+
             Debug.Log(req.downloadHandler.text);
 
             //ëóêM
@@ -35,5 +44,16 @@ public class HttpRequest : MonoBehaviour
             //    request.SetRequestHeader("Content-Type", "application/json");
             //    yield return request.Send();
         }
+    }
+}
+
+public class Data
+{
+    public int age;
+    public string name;
+    public Data(string name,int age)
+    {
+        this.age = age;
+        this.name = name;
     }
 }
