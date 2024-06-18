e121323b0ae3879387ed90871bb769b90ca43fba
STD\s20227002 <shironeko248@gmail.com>
2024-06-18 15:03:16 +0900

    投擲物の初期化実装開始

--- Assets/Scripts/NishigakiScripts/ThrowData.cs
+++ Assets/Scripts/NishigakiScripts/ThrowData.cs
@@ -7,11 +7,44 @@ using UnityEngine;
 /// </summary>
 public class ThrowData
 {
-
-    public ThrowData()
+    public ThrowData(Vector3 nowPosition)
     {
-
+        // 
+        _throwObjectOrbitPositions[0] = nowPosition;
+        _throwObjectOrbitPositions[1] = nowPosition;
+        _throwObjectOrbitPositions[2] = nowPosition;
     }
 
-    private Transform _throwObjectTransform = default;
+    // 投擲オブジェクトがつかまれているときの軌道座標たちorbit
+    private Vector3[] _throwObjectOrbitPositions = new Vector3[3];
+
+    /// <summary>
+    /// 新しい軌道座標を保存するためのプロパティ
+    /// </summary>
+    /// <param name="newPosition">新しい軌道座標</param>
+    public void SetOrbitPosition(Vector3 newPosition)
+    {
+        // 保存してある座標の保存位置を更新する
+        _throwObjectOrbitPositions[2] = _throwObjectOrbitPositions[1];
+        _throwObjectOrbitPositions[1] = _throwObjectOrbitPositions[0];
+
+        // 新しい座標を保存する
+        _throwObjectOrbitPositions[0] = newPosition;
+    }
+
+    public void GetThrowVelocity()
+    {
+        // 保存してある軌道座標から投擲用ベクトルを作成する --------------------------------
+        // 一つ目
+        Vector3 firstVector = _throwObjectOrbitPositions[0] - _throwObjectOrbitPositions[1];
+
+        // 二つ目
+        Vector3 secondVector = _throwObjectOrbitPositions[1] - _throwObjectOrbitPositions[2];
+        // ---------------------------------------------------------------------------------
+
+        // 投擲用ベクトルのノルムの平均から投擲速度を作成する
+        float magnitude = (firstVector.magnitude + secondVector.magnitude) / 2;
+
+
+    }
 }
---
+++ Assets/Scripts/NishigakiScripts/Throwable.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwable : MonoBehaviour
+{
+    /// <summary>
+    /// ‚Â‚©‚Ü‚ê‚½‚Æ‚«‚ÉÀs‚·‚éˆ—
+    /// </summary>
+    public void Select()
+    {
+
+    }
+
+    /// <summary>
+    /// —£‚³‚ê‚½‚Æ‚«‚ÉÀs‚·‚éˆ—
+    /// </summary>
+    public void UnSelect()
+    {
+
+    }
+}
