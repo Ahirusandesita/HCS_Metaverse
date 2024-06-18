976b7cb7efb57c31fc75b9f511f985cf53b2b84e
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2024-06-18 11:16:03 +0900

    追加 >> コメント

--- Assets/Scripts/IInteraction.cs
+++ Assets/Scripts/IInteraction.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
 
+/// <summary>
+/// インタラクトされるオブジェクトのインターフェース
+/// </summary>
 public interface IInteraction
 {
     GameObject gameObject { get; }
     ISelectedNotification SelectedNotification { get; }
+    /// <summary>
+    /// オブジェクトがインタラクトされたときに呼ばれる処理
+    /// <br>多くの場合、プレイヤーがオブジェクトのコライダーに触れたときに呼ばれる</br>
+    /// </summary>
     void Open();
+    /// <summary>
+    /// オブジェクトのインタラクト状態から離れるときに呼ばれる処理
+    /// <br>多くの場合、プレイヤーがオブジェクトのコライダーから離れたときに呼ばれる</br>
+    /// </summary>
     void Close();
 }
--- Assets/Scripts/SafetyInteractionObject.cs
+++ Assets/Scripts/SafetyInteractionObject.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/// <summary>
+/// 二重のインタラクトを実装するオブジェクトの基底クラス
+/// <br>（コライダー内に入り、かつ入力が必要）</br>
+/// </summary>
 public abstract class SafetyInteractionObject : MonoBehaviour, IInteraction, ISelectedNotification
 {
     ISelectedNotification IInteraction.SelectedNotification => this;
@@ -8,35 +12,49 @@ public abstract class SafetyInteractionObject : MonoBehaviour, IInteraction, ISe
 
     protected virtual void Awake()
     {
+        // Interact入力の購読
         Interaction.Interact.performed += _ =>
         {
             SafetyOpen();
             Interaction.Interact.Disable();
             Interaction.Disengage.Enable();
+            // プレイヤーの基本操作を停止（移動、ジャンプ、転回）
             Player.Disable();
         };
+        // Disengage入力の購読
         Interaction.Disengage.performed += _ =>
         {
             SafetyClose();
             Interaction.Interact.Enable();
             Interaction.Disengage.Disable();
+            // プレイヤーの基本操作を再開（移動、ジャンプ、転回）
             Player.Enable();
         };
     }
 
     void IInteraction.Open()
     {
+        // UIを表示
         NotificationUIManager.Instance.DisplayInteraction();
         Interaction.Interact.Enable();
     }
 
     void IInteraction.Close()
     {
+        // UIを非表示
         NotificationUIManager.Instance.HideInteraction();
         Interaction.Disable();
     }
 
+    /// <summary>
+    /// オブジェクトがインタラクトされたときに呼ばれる処理
+    /// <br>プレイヤーがオブジェクトの範囲上で入力をしたときに呼ばれる</br>
+    /// </summary>
     protected abstract void SafetyOpen();
+    /// <summary>
+    /// オブジェクトのインタラクト状態から離れるときに呼ばれる処理
+    /// <br>プレイヤーがオブジェクトの範囲上で入力をしたときに呼ばれる</br>
+    /// </summary>
     protected abstract void SafetyClose();
 
     public abstract void Select(SelectArgs selectArgs);
