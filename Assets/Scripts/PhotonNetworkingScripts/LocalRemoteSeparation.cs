c50421eb61c2c8da270c1b2e3dcd1214268ef8f2
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-06-26 14:53:31 +0900

    no message

--- Assets/Scripts/PhotonNetworkingScripts/LocalRemoteSeparation.cs
+++ Assets/Scripts/PhotonNetworkingScripts/LocalRemoteSeparation.cs
@@ -12,13 +12,10 @@ public class LocalRemoteSeparation : MonoBehaviour
     [SerializeField]
     private NetworkPrefabRef remoteViewObject;
 
-    // Update is called once per frame
-    public void RemoteViewCreate(NetworkRunner networkRunner,PlayerRef playerRef)
+	public void RemoteViewCreate(NetworkRunner networkRunner,PlayerRef playerRef)
     {                                   //FusionÇÃê∂ê¨
         RemoteView remoteView = networkRunner.Spawn(remoteViewObject,Vector3.zero,Quaternion.identity,playerRef).GetComponent<RemoteView>();
 
         Instantiate(separationLifetimeScope).SeparationSetup(localGameObject, remoteView).Build();
-
-        Destroy(this);
     }
 }
