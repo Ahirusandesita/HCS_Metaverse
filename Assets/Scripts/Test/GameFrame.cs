f86df75952085a8a7777120bec7a65ef11401301
STD\s20227005 <Ahirusandesita>
2024-06-26 13:58:34 +0900

    ふざけんな仕事ふやしやがってこれ用のシステムじゃねぇんだよ！

--- Assets/Scripts/Test/GameFrame.cs
+++ Assets/Scripts/Test/GameFrame.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameFrame : MonoBehaviour
 {
@@ -28,6 +29,8 @@ public class GameFrame : MonoBehaviour
         }
 
         this.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        SceneManager.LoadScene("CookActivity");
     }
     IEnumerator FrameAnimationDown()
     {
