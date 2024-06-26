5319451bb9887ecfeebda72c7d109064b9842759
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-06-26 12:13:02 +0900

    fusionSetUp

--- Assets/Scripts/PhotonNetworkingScripts/MasterServerConect.cs
+++ Assets/Scripts/PhotonNetworkingScripts/MasterServerConect.cs
@@ -1,28 +1,132 @@
 using UnityEngine;
+using Fusion;
+using Fusion.Sockets;
+using System.Collections.Generic;
+using System;
 
 public interface IMasterServerConectable
 {
     void Connect();
 }
 
-public class MasterServerConect : MonoBehaviour/*MonoBehaviourPunCallbacks,*/, IMasterServerConectable
+public class MasterServerConect : NetworkBehaviour,INetworkRunnerCallbacks, IMasterServerConectable
 {
     [SerializeField]
     private LocalRemoteSeparation localRemoteReparation;
 
-    //public override void OnConnectedToMaster()
-    //{
-    //    // "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
-    //    PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions(), TypedLobby.Default);
-    //}
 
-    //public override void OnJoinedRoom()
-    //{
-    //    localRemoteReparation.RemoteViewCreate();
-    //}
+	[SerializeField]
+	private NetworkRunner _networkRunner;
 
-    void IMasterServerConectable.Connect()
+	private void Start()
+	{
+		_networkRunner.AddCallbacks(this);		
+	}
+
+	void IMasterServerConectable.Connect()
     {
-        //PhotonNetwork.ConnectUsingSettings();
-    }
+		// "Room"という名前のルームに参加する（ルームが存在しなければ作成して参加する）
+		_networkRunner.StartGame(new StartGameArgs
+		{
+			GameMode = GameMode.AutoHostOrClient,
+			SessionName = "Room",
+			SceneManager = _networkRunner.GetComponent<NetworkSceneManagerDefault>()
+		}
+		);
+	}
+
+	public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+	{
+		localRemoteReparation.RemoteViewCreate(runner,player);
+	}
+
+	public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnInput(NetworkRunner runner, NetworkInput input)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnConnectedToServer(NetworkRunner runner)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnSceneLoadDone(NetworkRunner runner)
+	{
+		throw new NotImplementedException();
+	}
+
+	public void OnSceneLoadStart(NetworkRunner runner)
+	{
+		throw new NotImplementedException();
+	}
 }
