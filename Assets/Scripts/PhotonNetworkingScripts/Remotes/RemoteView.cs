adafd38a08a5f867ff098ae003165aa4b155e095
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-06-27 14:28:14 +0900

    RemoteViewにメソッド追加

--- Assets/Scripts/PhotonNetworkingScripts/Remotes/RemoteView.cs
+++ Assets/Scripts/PhotonNetworkingScripts/Remotes/RemoteView.cs
@@ -1,11 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Fusion;
 
 public class RemoteView : MonoBehaviour
 {
-    public void SetVector3(Vector3 vector)
-	{
+	private Transform myTransform = default;
 
+	private MasterServerConect _masterServerConect = default;
+	private void Start()
+	{
+		myTransform = transform;
+		_masterServerConect = (MasterServerConect)FindObjectOfType(typeof(MasterServerConect));
+
+	}
+
+	/// <summary>
+	/// ホストだったら名前変えてシーンを読み込む
+	/// </summary>
+	public void ActivityStart()
+	{
+		
+	}
+
+	/// <summary>
+	/// ルームに入る。ない場合は作る
+	/// </summary>
+	/// <param name="sessionName">セッション名</param>
+	public void JoinRoom(string sessionName)
+	{
+		_masterServerConect.UpdateNetworkRunner();
+
+		_masterServerConect.Runner.StartGame(new StartGameArgs
+		{
+			GameMode = GameMode.AutoHostOrClient,
+			SessionName = sessionName,
+			SceneManager = _masterServerConect.Runner.GetComponent<NetworkSceneManagerDefault>()
+		});
+	}
+
+	public void SetVector3(Vector3 vector)
+	{
+		Debug.LogWarning(vector);
+
+		myTransform.position = vector;
 	}
 }
