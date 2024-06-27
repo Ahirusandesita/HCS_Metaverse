0936530ff52e0a80c37859e82e6e88a94798f8ae
STD\s20227005 <Ahirusandesita>
2024-06-27 14:03:29 +0900

    ActivityZone

--- Assets/Scripts/Activitys/ActivityZone.cs
+++ Assets/Scripts/Activitys/ActivityZone.cs
@@ -1,6 +1,13 @@
 using UnityEngine;
-
+using Fusion;
 public class ActivityZone : MonoBehaviour
 {
-    
+    private string sessionName;
+    public string SessionName => sessionName;
+
+    [Rpc]
+    private void A(string sessionName)
+    {
+        
+    }
 }
