5602d4ac80cf1adf9bbaa711a286c44586a7588f
STD\s20227002 <shironeko248@gmail.com>
2024-06-27 11:56:56 +0900

    手の座標をもとに投げれるようにしてみた　動くかどうかは運次第

--- Assets/Scripts/NishigakiScripts/Throwable.cs
+++ Assets/Scripts/NishigakiScripts/Throwable.cs
@@ -12,16 +12,29 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
     [SerializeField, Tooltip("自身が持つTransform")]
     public Transform _thisTransform = default;
 
-    // 掴んでいる手のTransform
+    // 現在掴んでいる手のTransform
+    private Transform _grabbingHandTransform = default;
+
+    // 右手のTransform
     private Transform _rightHandTransform = default;
 
-    // 掴んでいる手の種類
-    private HandType _handType = default;
+    // 左手のTransform
+    private Transform _leftHandTransform = default;
+
+    // 掴んだ時に渡される手の方向を格納する変数
+    private HandType _detailEventsHandType = default;
 
     // 使用中のThrowDataを格納するための変数
     public ThrowData _throwData = default;
 
-    InteractorDetailEventIssuer _interactorDetailEventIssuer ;
+    // 現在右手で掴んでいるかどうか
+    private bool _isGrabbingRightHand = false;
+
+    // 現在左手で掴んでいるかどうか
+    private bool _isGrabbingLeftHand = false;
+
+    // 
+    private InteractorDetailEventIssuer _interactorDetailEventIssuer ;
 
     private void Awake()
     {
@@ -31,29 +44,23 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
 
     private void Start()
     {
-        PlayerInitialize.ConsignmentInject_static(this);
-
-        // 
+        // 掴んだ時の情報を講読できるようにする
         _interactorDetailEventIssuer.OnInteractor += (handler) => { 
-            _handType = handler.HandType; 
-            
+            _detailEventsHandType = handler.HandType;
         };
-
-        // 
-
     }
 
     private void FixedUpdate()
     {
-        // つかんでいる時のみ実行する
-        if (_throwData is null)
+        // どちらの手でも掴んでいない場合
+        if (!_isGrabbingRightHand && !_isGrabbingLeftHand)
         {
-            // つかんでいなかったら何もしない
+            // 何もしない
             return;
         }
 
         // 現在の座標を保存する
-        _throwData.SetOrbitPosition(_thisTransform.position);
+        _throwData.SetOrbitPosition(_grabbingHandTransform.position);
     }
 
     /// <summary>
@@ -61,26 +68,21 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
     /// </summary>
     public void Select()
     {
-        // 
-        Transform grabbingHandTransform = default;
-
-        // 掴んだ手の種類を判断する
-        if (_handType == HandType.Right)
+        // 掴んだ時の手の情報がなかった場合
+        if (GetDetailHandsTransform(_detailEventsHandType) is null)
         {
-            // 右手を設定する
-            grabbingHandTransform = _rightHandTransform;
+            // 何もしない
+            return;
         }
-        else if (_handType == HandType.Left)
-        {
 
-        }
-        else
-        {
+        // 掴んだ手の方向をもとにフラグを立てる
+        SetGrabbingHandFlag(_detailEventsHandType, true);
 
-        }
+        // 掴んだ時のTransformを現在掴んでいる手として登録する
+        _grabbingHandTransform = GetDetailHandsTransform(_detailEventsHandType) ;
 
         // 情報の初期化を行う
-        _throwData.ReSetThrowData(_thisTransform.position);
+        _throwData.ReSetThrowData(_grabbingHandTransform.position);
     }
 
     /// <summary>
@@ -88,6 +90,34 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
     /// </summary>
     public void UnSelect()
     {
+        // 掴んだ手の方向をもとにフラグを消す
+        SetGrabbingHandFlag(_detailEventsHandType, false);
+
+        // まだどちらかの手で掴み続けていた場合
+        if (_isGrabbingRightHand || _isGrabbingLeftHand)
+        {
+            // まだつかんでいる方の手のTransform
+            Transform nowGrabbingHand = default;
+
+            // まだ掴んでいる方の手の情報を判別する
+            if (_isGrabbingRightHand)
+            {
+                // 右手のTransformを代入
+                nowGrabbingHand = GetDetailHandsTransform(HandType.Right);
+            }
+            else
+            {
+                // 左手のTransformを代入
+                nowGrabbingHand = GetDetailHandsTransform(HandType.Left);
+            }
+
+            // まだ掴んでいる手の情報で初期化を行う
+            _throwData.ReSetThrowData(nowGrabbingHand.position);
+
+            // 投げないで終了する
+            return;
+        }
+
         // Kinematicを無効にする
         _thisRigidbody.isKinematic = false;
 
@@ -96,6 +126,58 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
 
         // 1フレーム後にベクトルを上書きする
         StartCoroutine(OverwriteVelocity(throwVector));
+    }
+
+    /// <summary>
+    /// 掴んでいる手のフラグを変更するためのメソッド
+    /// </summary>
+    /// <param name="handType">手の方向</param>
+    /// <param name="setState">変更する値</param>
+    private void SetGrabbingHandFlag(HandType handType, bool setState)
+    {
+        // 掴んだ手の方向をもとにフラグを変更する
+        switch (handType)
+        {
+            // 右手の場合
+            case HandType.Right:
+                // 右手の掴んでいるかどうかのフラグを変更する
+                _isGrabbingRightHand = setState;
+                break;
+
+            // 左手の場合
+            case HandType.Left:
+                // 左手の掴んでいるかどうかのフラグを変更する
+                _isGrabbingLeftHand = setState;
+                break;
+
+            // 例外処理
+            default:
+                // 何もしない
+                Debug.LogError($"SetGrabbingHandFlagにて手の方向に例外が発生　手の方向：{handType}");
+                return;
+        }
+    }
+
+    private Transform GetDetailHandsTransform(HandType handType)
+    {
+        switch (handType)
+        {
+            // 右手の場合
+            case HandType.Right:
+                // 右手のTransformを返す
+                return _rightHandTransform;
+
+            // 左手の場合
+            case HandType.Left:
+                // 左手のTransformを返す
+                return _leftHandTransform;
+
+            // 例外処理
+            default:
+                // 何も返さない
+                Debug.LogError($"GetDetailHandsTransformにて手の方向に例外が発生　手の方向：{handType}");
+                return null;
+        }
     }
 
     private IEnumerator OverwriteVelocity(Vector3 throwVector)
@@ -109,7 +191,8 @@ public class Throwable : MonoBehaviour, IDependencyInjector<PlayerHandDependency
 
     public void Inject(PlayerHandDependencyInfomation information)
     {
-        // 
-        _rightHandTransform = information.RightHand.transform;
+        // 手のTransformを登録する
+        _rightHandTransform = information.RightHand;
+        _leftHandTransform = information.LeftHand;
     }
 }
