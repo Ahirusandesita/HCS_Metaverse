151c3a35414ff2cecd6b5ae98cbadda26f7b26dc
STD\s20227005 <Ahirusandesita>
2024-07-03 10:21:29 +0900

    Commodity仮完成

--- Assets/Scripts/Activitys/Cook/Player/Chef.cs
+++ Assets/Scripts/Activitys/Cook/Player/Chef.cs
@@ -6,10 +6,12 @@ public class Chef : MonoBehaviour
 {
     [SerializeField]
     private Commodity commodity;
+    [SerializeField]
+    private Commodity burger;
     // Start is called before the first frame update
     void Start()
     {
-        this.GetComponent<MixProcessedGoods>().Mix(new Commodity[] { commodity, commodity });
+        this.GetComponent<MixProcessedGoods>().Mix(new Commodity[] { commodity, burger });
     }
 
     // Update is called once per frame
