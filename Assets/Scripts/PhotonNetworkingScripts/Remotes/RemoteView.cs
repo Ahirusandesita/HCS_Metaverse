34b0bfef2d2f52aac68369607e06f361fcaee6fb
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-07-03 09:52:01 +0900

    no message

--- Assets/Scripts/PhotonNetworkingScripts/Remotes/RemoteView.cs
+++ Assets/Scripts/PhotonNetworkingScripts/Remotes/RemoteView.cs
@@ -1,41 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
 public class RemoteView : MonoBehaviour
 {
-	private Transform myTransform = default;
-
-	private MasterServerConect _masterServerConect = default;
 	[SerializeField]
 	private RPCManager _rpcManager = default;
+	private Transform myTransform = default;
+	private NetworkRunner _networkRunner = default;
+	private MasterServerConect _masterServerConect = default;
+
 	private void Start()
 	{
 		myTransform = transform;
 		_masterServerConect = (MasterServerConect)FindObjectOfType(typeof(MasterServerConect));
 		_rpcManager = (RPCManager)FindObjectOfType(typeof(RPCManager));
+		_networkRunner = _rpcManager.Runner;
 	}
 
-	[ContextMenu("dadadad")]
 	/// <summary>
-	/// ホストだったら名前変えてシーンを読み込む
+	/// アクティビティを開始する
 	/// </summary>
-	public void ActivityStart()
+	/// <param name="sceneName">開始するアクティビティのシーン名</param>
+	public void ActivityStart(string sceneName)
 	{
+		if (!_networkRunner.IsServer) { return; }
+
 		_rpcManager.Rpc_SessionNaming("dadadad");
 		Debug.LogWarning("ActivityStart");
+		_networkRunner.LoadScene(sceneName);
 	}
 
 	/// <summary>
 	/// ルームに入る。ない場合は作る
 	/// </summary>
 	/// <param name="sessionName">セッション名</param>
-	public void JoinRoom(string sessionName)
+	public async void JoinOrCreateRoom(string sessionName)
 	{
-		_masterServerConect.UpdateNetworkRunner();
+		if (_networkRunner.IsServer)
+		{
+			_masterServerConect.UpdateNetworkRunner();
+		}
 
-		_masterServerConect.Runner.StartGame(new StartGameArgs
+		await _masterServerConect.Runner.StartGame(new StartGameArgs
 		{
 			GameMode = GameMode.AutoHostOrClient,
 			SessionName = sessionName,
