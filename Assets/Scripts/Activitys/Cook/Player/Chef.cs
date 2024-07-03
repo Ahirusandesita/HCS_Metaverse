942b5828286951d6cea7b47e6f530e9f3bb67c2d
STD\s20227002 <shironeko248@gmail.com>
2024-07-03 15:40:18 +0900

    食材にょっき

--- Assets/Scripts/Activitys/Cook/Player/Chef.cs
+++ Assets/Scripts/Activitys/Cook/Player/Chef.cs
@@ -5,22 +5,47 @@ using UnityEngine;
 public class Chef : MonoBehaviour
 {
     [SerializeField]
-    private Commodity commodity;
+    private Ingrodients tomato;
+
     [SerializeField]
-    private Commodity burger;
+    private Ingrodients rawMeat;
+
     [SerializeField]
-    private Ingrodients ingrodients;
+    private Commodity banzu;
+    [SerializeField]
+    private Commodity cutTomato;
+    [SerializeField]
+    private Ingrodients cutMeat;
+    [SerializeField]
+    private Commodity fireMeat;
+    
     // Start is called before the first frame update
     void Start()
     {
         //Instantiate(MixCommodity.Mix(new Commodity[] { commodity, burger }));
-
-        ingrodients.ProcessingStart(ProcessingType.Bake);
+        StartCoroutine(A());
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            cutMeat.ProcessingStart(ProcessingType.Bake);
+        }
 
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(MixCommodity.Mix(new Commodity[] { fireMeat, cutTomato, banzu }));
+        }
+    }
+
+    IEnumerator A()
+    {
+        yield return new WaitForSeconds(1f);
+
+        tomato.ProcessingStart(ProcessingType.Cut);
+        rawMeat.ProcessingStart(ProcessingType.Cut);
     }
 }
