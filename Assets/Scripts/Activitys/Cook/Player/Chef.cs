72f6ad7df2fbf951fc8b7ebb97ba9e5203714225
STD\s20227005 <Ahirusandesita>
2024-07-03 16:07:56 +0900

    no message

--- Assets/Scripts/Activitys/Cook/Player/Chef.cs
+++ Assets/Scripts/Activitys/Cook/Player/Chef.cs
@@ -7,45 +7,8 @@ public class Chef : MonoBehaviour
     [SerializeField]
     private Ingrodients tomato;
 
-    [SerializeField]
-    private Ingrodients rawMeat;
-
-    [SerializeField]
-    private Commodity banzu;
-    [SerializeField]
-    private Commodity cutTomato;
-    [SerializeField]
-    private Ingrodients cutMeat;
-    [SerializeField]
-    private Commodity fireMeat;
-    
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        //Instantiate(MixCommodity.Mix(new Commodity[] { commodity, burger }));
-        StartCoroutine(A());
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            cutMeat.ProcessingStart(ProcessingType.Bake);
-        }
-
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Instantiate(MixCommodity.Mix(new Commodity[] { fireMeat, cutTomato, banzu }));
-        }
-    }
-
-    IEnumerator A()
-    {
-        yield return new WaitForSeconds(1f);
-
-        tomato.ProcessingStart(ProcessingType.Cut);
-        rawMeat.ProcessingStart(ProcessingType.Cut);
+        GameObject.FindObjectOfType<AutoMachine>().ProcessedCertification(tomato).Processing(tomato);
     }
 }
