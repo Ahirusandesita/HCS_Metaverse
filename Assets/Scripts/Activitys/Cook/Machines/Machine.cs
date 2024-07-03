3780507d2f3e8d685ad72d7b847d350999ba3962
STD\s20227005 <Ahirusandesita>
2024-07-03 16:07:47 +0900

    Machine　Pan

--- Assets/Scripts/Activitys/Cook/Machines/Machine.cs
+++ Assets/Scripts/Activitys/Cook/Machines/Machine.cs
@@ -1,6 +1,48 @@
+using System.Collections.Generic;
 using UnityEngine;
-
-public class Machine : MonoBehaviour
+public interface IProcessable
 {
-    
+    void Processing(Ingrodients ingrodients);
+}
+public class NullMachine : IProcessable
+{
+    void IProcessable.Processing(Ingrodients ingrodients)
+    {
+
+    }
+}
+
+public abstract class Machine : MonoBehaviour,IProcessable
+{
+    [SerializeField]
+    private ProcessingType processingType;
+    public ProcessingType ProcessingType => processingType;
+
+    [SerializeField]
+    private Transform ingrodientTransform;
+
+    protected Ingrodients ingrodients;
+    protected float timeItTakes;
+
+    void IProcessable.Processing(Ingrodients ingrodients)
+    {
+        this.ingrodients = ingrodients;
+        ingrodients.transform.position = ingrodientTransform.position;
+        StartProcessed();
+    }
+
+    public IProcessable ProcessedCertification(Ingrodients ingrodients)
+    {
+        foreach(IngrodientsDetailInformation item in ingrodients.IngrodientsAsset.IngrodientsDetailInformations)
+        {
+            if(item.ProcessingType == processingType)
+            {
+                return this;
+            }
+        }
+        Debug.Log("can not processed");
+        return new NullMachine();
+    }
+
+    public abstract void StartProcessed();
 }
