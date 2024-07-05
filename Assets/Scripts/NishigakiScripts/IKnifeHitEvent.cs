6fe39d81a2fe944f0dd8659e92f72faf1bb49943
STD\s20227002 <shironeko248@gmail.com>
2024-07-05 13:44:26 +0900

    包丁止まるようになったり切れるようになったかも　できてたらうれしいな

---
+++ Assets/Scripts/NishigakiScripts/IKnifeHitEvent.cs
@@ -0,0 +1,4 @@
+public interface IKnifeHitEvent
+{
+    public void KnifeHitEvent();
+}
---
+++ Assets/Scripts/NishigakiScripts/TestMANAITA.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMANAITA : MonoBehaviour, IKnifeHitEvent
+{
+    [SerializeField, Tooltip("きるよん！！！！！！！")]
+    private Ingrodients _cutObject = default;
+
+    void IKnifeHitEvent.KnifeHitEvent()
+    {
+        _cutObject.ProcessingStart(ProcessingType.Cut);
+    }
+}
