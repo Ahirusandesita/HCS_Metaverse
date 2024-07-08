bdb6b175d72fa5e3566df7c07c1a7c86592e1970
STD\s20227002 <shironeko248@gmail.com>
2024-07-08 15:02:14 +0900

    たぶんできてるっぽいストッパー

--- Assets/Scripts/NishigakiScripts/Stoppable.cs
+++ Assets/Scripts/NishigakiScripts/Stoppable.cs
@@ -7,6 +7,9 @@ public class Stoppable : MonoBehaviour
     [SerializeField, Tooltip("見た目用オブジェクトのTransform")]
     private Transform _viewObjectTransform = default;
 
+    [SerializeField, Tooltip("IStoppingEventを持つGameObject")]
+    private GameObject _stoppingEventObject = default;
+
     private InteractorDetailEventIssuer _detailEventIssuer = default;
 
     private HandType _detailEventsHandType = default;
@@ -15,13 +18,37 @@ public class Stoppable : MonoBehaviour
 
     public HandType GetDetailHandType => _detailEventsHandType;
 
+    private IStoppingEvent _iStoppingEvent = default;
+
+    public void StoppingEvent()
+    {
+        if (_iStoppingEvent != default)
+        {
+            // 
+            _iStoppingEvent.StoppingEvent();
+        }
+    }
+
     private void Start()
     {
+        // 
+        if (_stoppingEventObject.TryGetComponent<IStoppingEvent>(out var iStoppingEvent))
+        {
+            // 
+            _iStoppingEvent = iStoppingEvent;
+        }
+        else
+        {
+            // 
+            Debug.LogError($"<color=green>{this.name} </color>に IStoppingEventが付いていないからバグるよ");
+        }
+
         // 
         _detailEventIssuer = GameObject.FindObjectOfType<InteractorDetailEventIssuer>();
 
         // 掴んだ時の情報を講読できるようにする
-        _detailEventIssuer.OnInteractor += (handler) => {
+        _detailEventIssuer.OnInteractor += (handler) =>
+        {
             _detailEventsHandType = handler.HandType;
         };
     }
