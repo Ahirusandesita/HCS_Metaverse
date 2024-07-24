46b33eff3db27e6c462353d578e95c6bf992aaea
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2024-07-24 16:21:06 +0900

    追加 >> 拡張メソッドによるprint

--- Assets/Scripts/Other/XDebug.cs
+++ Assets/Scripts/Other/XDebug.cs
@@ -10,10 +10,33 @@ public static class XDebug
 #endif
     }
 
+    /// <param name="color">「#」から始まるカラーコード、または「red」等HTMLのカラーネーム</param>
     public static void LogColor(object message, string color = "white")
     {
 #if UNITY_EDITOR
         Debug.Log($"<color={color}>{message}</color>");
+#endif
+    }
+
+    public static void Print(this object message)
+    {
+#if UNITY_EDITOR
+        Debug.Log(message);
+#endif
+    }
+
+    public static void PrintColor(this object message, Color color)
+    {
+#if UNITY_EDITOR
+        LogColor(message, color);
+#endif
+    }
+
+    /// <param name="color">「#」から始まるカラーコード、または「red」等HTMLのカラーネーム</param>
+    public static void PrintColor(this object message, string color = "white")
+    {
+#if UNITY_EDITOR
+        LogColor(message, color);
 #endif
     }
 }
