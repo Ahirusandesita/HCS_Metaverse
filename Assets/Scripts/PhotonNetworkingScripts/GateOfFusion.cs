154ce44c2fdc7cc8f60a2e8257ab0f458af4c75c
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-07-24 15:11:41 +0900

    no message

--- Assets/Scripts/PhotonNetworkingScripts/GateOfFusion.cs
+++ Assets/Scripts/PhotonNetworkingScripts/GateOfFusion.cs
@@ -7,17 +7,45 @@ public class GateOfFusion
 {
 	private NetworkRunner _networkRunner = default;
 	private static GateOfFusion _instance = default;
+	private MasterServerConect _masterServer = default;
 	public static GateOfFusion Instance => _instance ??= new GateOfFusion();
+	private MasterServerConect MasterServer => _masterServer ??= Object.FindObjectOfType<MasterServerConect>();
 
-	public NetworkRunner NetworkRunner 
+	public NetworkRunner NetworkRunner
 	{
 		get
 		{
-			if(_networkRunner == null)
+			if (_networkRunner == null)
 			{
 				_networkRunner = Object.FindObjectOfType<NetworkRunner>();
 			}
 			return _networkRunner;
 		}
+	}
+
+	/// <summary>
+	/// 掴むときに呼ぶ
+	/// </summary>
+	/// <param name="networkObject">掴んだオブジェクト</param>
+	public void Grab(NetworkObject networkObject)
+	{
+		Debug.LogWarning($"Grab:");
+		StateAuthorityData stateAuthorityData = networkObject.GetComponent<StateAuthorityData>();
+		if (stateAuthorityData.IsNotReleaseStateAuthority)
+		{
+			Debug.LogWarning($"<color=red>権限がありませんでした</color>");
+			return;
+		}
+		if (networkObject.HasStateAuthority)
+		{
+			Debug.LogWarning("<color=lime>自分が権限を持っています</color>");
+			return;
+		}
+		networkObject.RequestStateAuthority();
+	}
+
+	public void Release(NetworkObject networkObject)
+	{
+
 	}
 }
