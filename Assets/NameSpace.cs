91060ef6364cb680fc1aee56b93b29b75b850af8
STD\s20227005 <Ahirusandesita>
2024-08-21 10:17:53 +0900

    NameSpace

---
+++ Assets/NameSpace.cs
@@ -0,0 +1,20 @@
+namespace HCSMeta
+{
+
+    namespace Activity
+    {
+        namespace Shop { }
+        namespace Cook { }
+        namespace Fishing { }
+    }
+
+
+    namespace Player
+    {
+        namespace VR { }
+    }
+
+    namespace Function { }
+
+    namespace Network { }
+}
