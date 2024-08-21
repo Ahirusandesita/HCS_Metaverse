5766b154ca733e239fbcd7169f04277861759b35
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-08-21 13:38:03 +0900

    no message

---
+++ Assets/Scripts/PhotonNetworkingScripts/ExtensionList.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using HCSMeta.Network;
+using Fusion;
+
+public static partial class ExtensionList
+{
+	public static int IndexOf(this List<RoomPlayer> list, PlayerRef playerRef)
+	{
+		for (int i = 0; i < list.Count; i++)
+		{
+			if (list[i].PlayerData != playerRef) { continue; }
+			return i;
+		}
+		return -1;
+	}
+}
