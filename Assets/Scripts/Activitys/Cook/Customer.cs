9bf8d00a28f67064d4483786058e5956291bdb51
STD\s20227002 <shironeko248@gmail.com>
2024-08-22 14:32:33 +0900

    おほん

--- Assets/Scripts/Activitys/Cook/Customer.cs
+++ Assets/Scripts/Activitys/Cook/Customer.cs
@@ -2,37 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace HCSMeta.Activity.Cook
+public class Customer : MonoBehaviour
 {
-    public class Customer : MonoBehaviour
+    [SerializeField]
+    private OrderAsset orderAsset;
+    [SerializeField]
+    private OrderManager orderManager;
+    private RemoteOrder remoteOrder;
+    public void Order(int index)
     {
-        [SerializeField]
-        private OrderAsset orderAsset;
-        [SerializeField]
-        private OrderManager orderManager;
-        private RemoteOrder remoteOrder;
-        public void Order(int index)
-        {
-            remoteOrder.RPC_Order(index);
-        }
+        remoteOrder.RPC_Order(index);
+    }
 
-        public void InjectRemoteOrder(RemoteOrder remoteOrder)
-        {
-            this.remoteOrder = remoteOrder;
-        }
+    public void InjectRemoteOrder(RemoteOrder remoteOrder)
+    {
+        this.remoteOrder = remoteOrder;
+    }
 
-        private void Update()
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
         {
-            if (Input.GetKeyDown(KeyCode.C))
-            {
-                Order(0);
-            }
+            Order(0);
         }
+    }
 
-        public void RemoteOrder(int index)
-        {
-            OrderTicket orderTicket = orderManager.Inquiry();
-            orderTicket.Orderable.Order(orderAsset.OrderDetailInformations[index].CommodityAsset, orderTicket.CustomerInformation);
-        }
+    public void RemoteOrder(int index)
+    {
+        OrderTicket orderTicket = orderManager.Inquiry();
+        orderTicket.Orderable.Order(orderAsset.OrderDetailInformations[index].CommodityAsset, orderTicket.CustomerInformation);
     }
 }
