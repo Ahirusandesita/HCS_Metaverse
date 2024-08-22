df89796c191170f7c9984122adb0d6ec8e84032e
STD\s20227002 <shironeko248@gmail.com>
2024-08-22 14:07:20 +0900

    namespaceばいばい

--- Assets/Scripts/Activitys/Cook/Machines/Puttable.cs
+++ Assets/Scripts/Activitys/Cook/Machines/Puttable.cs
@@ -1,42 +1,39 @@
 using UnityEngine;
 using Oculus.Interaction;
 
-namespace HCSMeta.Activity.Cook
+public class Puttable : MonoBehaviour
 {
-    public class Puttable : MonoBehaviour
+    private LockedCuttingBoard _parentLockedCuttingObject = default;
+
+    // 掴んだ時や離した時にイベントを実行するクラス
+    private PointableUnityEventWrapper _pointableUnityEventWrapper;
+
+    private void Awake()
     {
-        private LockedCuttingBoard _parentLockedCuttingObject = default;
+        _pointableUnityEventWrapper = this.GetComponentInChildren<PointableUnityEventWrapper>();
+        _pointableUnityEventWrapper.WhenSelect.AddListener((action) => { Select(); });
 
-        // 掴んだ時や離した時にイベントを実行するクラス
-        private PointableUnityEventWrapper _pointableUnityEventWrapper;
+    }
 
-        private void Awake()
-        {
-            _pointableUnityEventWrapper = this.GetComponentInChildren<PointableUnityEventWrapper>();
-            _pointableUnityEventWrapper.WhenSelect.AddListener((action) => { Select(); });
+    public void SetLockedCuttingObject(LockedCuttingBoard lockedCuttingObject)
+    {
+        // 
+        _parentLockedCuttingObject = lockedCuttingObject;
+    }
 
-        }
+    public void Select()
+    {
+        DestroyThis();
+    }
 
-        public void SetLockedCuttingObject(LockedCuttingBoard lockedCuttingObject)
-        {
-            // 
-            _parentLockedCuttingObject = lockedCuttingObject;
-        }
+    public void DestroyThis()
+    {
+        Destroy(this);
+    }
 
-        public void Select()
-        {
-            DestroyThis();
-        }
-
-        public void DestroyThis()
-        {
-            Destroy(this);
-        }
-
-        private void OnDestroy()
-        {
-            _pointableUnityEventWrapper.WhenSelect.RemoveListener((action) => { Select(); });
-            _parentLockedCuttingObject.CanselCutting();
-        }
+    private void OnDestroy()
+    {
+        _pointableUnityEventWrapper.WhenSelect.RemoveListener((action) => { Select(); });
+        _parentLockedCuttingObject.CanselCutting();
     }
 }
