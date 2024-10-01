84544c97cd39bce239babf118971491810a16f1f
STD\s20225010 <wata.kuma@hotmail.co.jp>
2024-10-01 15:01:12 +0900

    no message

--- Assets/Scripts/Maps/Mark.cs
+++ Assets/Scripts/Maps/Mark.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Mark : MonoBehaviour
 {
+    [SerializeField]
+    RectTransform rectTransform;
     public void MarkViewPosition(Vector2 position)
     {
-
+        rectTransform.localPosition = position;
     }
     public void MarkOutCamera()
     {
