5e71a666427ce6e038d53241131b4e35e9f0dcad
STD\s20227005 <Ahirusandesita>
2024-10-02 09:58:52 +0900

    no message

--- Assets/Scripts/Activitys/Cook/RPC/IAction.cs
+++ Assets/Scripts/Activitys/Cook/RPC/IAction.cs
@@ -3,16 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using System;
-public interface IInjectPracticableRPCEvent
-{
-    void Inject(IPracticableRPCEvent practicableRPCEvent);
-}
-public interface IAction : IInjectPracticableRPCEvent
-{
-    void Action();
-}
-public interface IAction<T> : IInjectPracticableRPCEvent
-{
-    void Action(T t);
-}
+
 
