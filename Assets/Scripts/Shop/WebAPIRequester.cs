f1d82c5e7ee3a91875743d86c0852e84e7a1159b
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2025-01-14 13:39:47 +0900

    追加 >> WebAPI/マイルーム入室、ユーザー場所情報登録

--- Assets/Scripts/Shop/WebAPIRequester.cs
+++ Assets/Scripts/Shop/WebAPIRequester.cs
@@ -20,6 +20,11 @@ public class WebAPIRequester
 	private const string CONTENT_TYPE = "application/json";
 
 
+	#region Post/Get メソッド
+	/// <summary>
+	/// ショップ入店時のAPI通信
+	/// </summary>
+	/// <returns>・商品ラインナップ</returns>
 	public async UniTask<OnShopEntryData> PostShopEntry(int shopId)
 	{
 		WWWForm form = new WWWForm();
@@ -40,6 +45,13 @@ public class WebAPIRequester
 		return onShopEntryData;
 	}
 
+	/// <summary>
+	/// ショップ購入時のAPI通信
+	/// </summary>
+	/// <returns>・購入後の所持品リスト
+	/// <br>・購入後の金額</br>
+	/// <br>・ショップの在庫リスト</br>
+	/// <br>・ユーザーID</br></returns>
 	public async UniTask<OnShopPaymentData> PostShopPayment(List<ItemStock> inventory, int shopId, int userId)
 	{
 		var sendShopPaymentData = new SendPaymentData(inventory, shopId, userId);
@@ -60,6 +72,12 @@ public class WebAPIRequester
 		return onShopPaymentData;
 	}
 
+	/// <summary>
+	/// 自販機アクセス時（マイルーム入室時の初期化など更新したいとき）のAPI通信
+	/// </summary>
+	/// <param name="shopId"></param>
+	/// <returns>・商品ラインナップ
+	/// <br>・自販機のアクティブ状況</br></returns>
 	public async UniTask<OnVMEntryData> PostVMEntry(int shopId)
 	{
 		WWWForm form = new WWWForm();
@@ -80,6 +98,14 @@ public class WebAPIRequester
 		return onVMEntryData;
 	}
 
+	/// <summary>
+	/// 自販機購入時のAPI通信
+	/// </summary>
+	/// <returns>・購入後の所持品リスト
+	/// <br>・購入後の金額</br>
+	/// <br>・自販機の在庫リスト</br>
+	/// <br>・ユーザーID</br>
+	/// <br>・自販機のアップデートフラグ</br></returns>
 	public async UniTask<OnVMPaymentData> PostVMPayment(int itemId, int shopId, int userId)
 	{
 		var itemList = new List<ItemStock>();
@@ -87,7 +113,7 @@ public class WebAPIRequester
 		var sendPaymentData = new SendPaymentData(itemList, shopId, userId);
 		string jsonData = JsonUtility.ToJson(sendPaymentData);
 
-		using var request = UnityWebRequest.Post(DETABASE_PATH_SHOP_BUY, jsonData, CONTENT_TYPE);
+		using var request = UnityWebRequest.Post(DETABASE_PATH_VENDINGMACHINE_BUY, jsonData, CONTENT_TYPE);
 		await request.SendWebRequest();
 		switch (request.result)
 		{
@@ -103,6 +129,57 @@ public class WebAPIRequester
 		return onVMPaymentData;
 	}
 
+	/// <summary>
+	/// マイルーム入室時のAPI通信
+	/// </summary>
+	/// <returns>・オブジェクトリスト
+	/// <br>・ショップID（自販機）</br></returns>
+	public async UniTask<OnMyRoomEntryData> PostMyRoomEntry(int userId)
+	{
+		WWWForm form = new WWWForm();
+		form.AddField("userId", userId);
+		using var request = UnityWebRequest.Post(DETABASE_PATH_MYROOM_ENTRY, form);
+		await request.SendWebRequest();
+
+		switch (request.result)
+		{
+			case Result.InProgress:
+				throw new System.InvalidOperationException("ネットワーク通信が未だ進行中。");
+
+			case Result.ConnectionError or Result.ProtocolError or Result.DataProcessingError:
+				throw new APIConnectException(request.error);
+		}
+
+		var onMyroomEntryData = JsonUtility.FromJson<OnMyRoomEntryData>(request.downloadHandler.text);
+		return onMyroomEntryData;
+	}
+
+	/// <summary>
+	/// ユーザー場所情報登録時のAPI通信
+	/// </summary>
+	/// <returns>なし</returns>
+	public async UniTask PostUserLocation(int userId, string sessionName, string locationName)
+	{
+		var sendUserLocationData = new SendUserLocationData(userId, sessionName, locationName);
+		string jsonData = JsonUtility.ToJson(sendUserLocationData);
+
+		using var request = UnityWebRequest.Post(DETABASE_PATH_USER_LOCATION, jsonData, CONTENT_TYPE);
+		await request.SendWebRequest();
+		switch (request.result)
+		{
+			case Result.InProgress:
+				throw new System.InvalidOperationException("ネットワーク通信が未だ進行中。");
+
+			case Result.ConnectionError or Result.ProtocolError or Result.DataProcessingError:
+				throw new APIConnectException(request.error);
+		}
+	}
+	#endregion
+
+	#region レスポンスデータ
+	/// <summary>
+	/// API通信の基底レスポンスデータ
+	/// </summary>
 	[System.Serializable]
 	public class ResponseData
 	{
@@ -113,7 +190,9 @@ public class WebAPIRequester
 		public string Message => message;
 	}
 
-
+	/// <summary>
+	/// ショップ入店時のレスポンスデータ
+	/// </summary>
 	[System.Serializable]
 	public class OnShopEntryData : ResponseData
 	{
@@ -139,6 +218,9 @@ public class WebAPIRequester
 		}
 	}
 
+	/// <summary>
+	/// ショップ購入時のレスポンスデータ
+	/// </summary>
 	[System.Serializable]
 	public class OnShopPaymentData : ResponseData
 	{
@@ -174,6 +256,9 @@ public class WebAPIRequester
 		}
 	}
 
+	/// <summary>
+	/// 自販機アクセス時のレスポンスデータ
+	/// </summary>
 	[System.Serializable]
 	public class OnVMEntryData : ResponseData
 	{
@@ -203,6 +288,9 @@ public class WebAPIRequester
 		}
 	}
 
+	/// <summary>
+	/// 自販機購入時のレスポンスデータ
+	/// </summary>
 	[System.Serializable]
 	public class OnVMPaymentData : ResponseData
 	{
@@ -241,6 +329,45 @@ public class WebAPIRequester
 		}
 	}
 
+	/// <summary>
+	/// マイルーム入室時のレスポンスデータ
+	/// </summary>
+	[System.Serializable]
+	public class OnMyRoomEntryData : ResponseData
+	{
+		public OnMyRoomEntryData(Body body)
+		{
+			this.body = body;
+		}
+
+		[SerializeField] private Body body = default;
+		public Body GetBody => body;
+
+		[System.Serializable]
+		public class Body
+		{
+			public Body(List<MyRoomObject> objectList, int shopId)
+			{
+				this.objectList = objectList;
+				this.shopId = shopId;
+			}
+
+			[SerializeField] private List<MyRoomObject> objectList = default;
+			[SerializeField] private int shopId = default;
+
+			public IReadOnlyList<MyRoomObject> ObjectList => objectList;
+			/// <summary>
+			/// 自販機が存在しない場合は-1
+			/// </summary>
+			public int ShopID => shopId;
+		}
+	}
+	#endregion
+
+	#region 送信データ
+	/// <summary>
+	/// 購入時の送信データ（内部パースに使用）
+	/// </summary>
 	[System.Serializable]
 	private class SendPaymentData
 	{
@@ -260,6 +387,37 @@ public class WebAPIRequester
 		public int UserID => userId;
 	}
 
+	/// <summary>
+	/// ユーザー場所情報登録時の送信データ
+	/// </summary>
+	[System.Serializable]
+	public class SendUserLocationData
+	{
+		public SendUserLocationData(int userId, string sessionName, string locationName)
+		{
+			this.userId = userId;
+			this.sessionName = sessionName;
+			this.locationName = locationName;
+		}
+
+		[SerializeField] private int userId = default;
+		[SerializeField] private string sessionName = default;
+		[SerializeField] private string locationName = default;
+
+		public int UserID => userId;
+		public string SessionName => sessionName;
+		public string LocationName => locationName;
+	}
+	#endregion
+
+	#region Struct
+	/// <summary>
+	/// ・アイテムID
+	/// <br>・価格</br>
+	/// <br>・割引率</br>
+	/// <br>・在庫数</br>
+	/// <br>・サイズ</br>
+	/// </summary>
 	[System.Serializable]
 	public struct ItemLineup
 	{
@@ -288,6 +446,10 @@ public class WebAPIRequester
 		public int Size => size;
 	}
 
+	/// <summary>
+	/// ・アイテムID
+	/// <br>・数</br>
+	/// </summary>
 	[System.Serializable]
 	public struct ItemStock
 	{
@@ -304,10 +466,52 @@ public class WebAPIRequester
 		public int Amount => amount;
 	}
 
+	/// <summary>
+	/// ・アイテムID
+	/// <br>・ハウジングID</br>
+	/// <br>・絶対座標</br>
+	/// <br>・EulerRotation</br>
+	/// </summary>
+	[System.Serializable]
+	public struct MyRoomObject
+	{
+		public MyRoomObject(int itemId, int housingId, Vector3 position, Vector3 eulerRotation)
+		{
+			this.itemId = itemId;
+			this.housingId = housingId;
+			x_position = position.x;
+			y_position = position.y;
+			z_position = position.z;
+			x_direction = eulerRotation.x;
+			y_direction = eulerRotation.y;
+			z_direction = eulerRotation.z;
+		}
+
+		[SerializeField] private int itemId;
+		[SerializeField] private int housingId;
+		[SerializeField] private float x_position;
+		[SerializeField] private float y_position;
+		[SerializeField] private float z_position;
+		[SerializeField] private float x_direction;
+		[SerializeField] private float y_direction;
+		[SerializeField] private float z_direction;
+
+		public int ItemID => itemId;
+		public int HousingID => housingId;
+		public Vector3 Position => new Vector3(x_position, y_position, z_position);
+		public Vector3 EulerRotation => new Vector3(x_direction, y_direction, z_direction);
+	}
+	#endregion
+
+	#region 例外
+	/// <summary>
+	/// ・API通信中の例外
+	/// </summary>
 	public class APIConnectException : System.Exception
 	{
 		public APIConnectException() : base() { }
 		public APIConnectException(string message) : base(message) { }
 		public APIConnectException(string message, System.Exception innerException) : base(message, innerException) { }
 	}
+	#endregion
 }
