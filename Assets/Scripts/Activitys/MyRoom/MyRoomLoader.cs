c7b7ef6d1a8a4c5d4f77f6b3c1f3963b25a17c22
STD\s20225010 <wata.kuma@hotmail.co.jp>
2025-01-14 11:50:34 +0900

    MyRoom本番できたー

---
+++ Assets/Scripts/Activitys/MyRoom/MyRoomLoader.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class MyRoomLoader : MonoBehaviour
+{
+	public async UniTask Load()
+	{
+		WebAPIRequester requester = new WebAPIRequester();
+
+		
+	}
+}
