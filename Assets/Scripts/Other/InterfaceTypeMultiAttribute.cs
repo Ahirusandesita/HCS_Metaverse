2da98eba12bb4c1d64d16d7bb0826eb778f8066d
STD\s20227082 <20227082-takayanagisora@hcs.ac.jp>
2025-01-15 15:50:40 +0900

    追加 >> 全家具のデータ

--- Assets/Scripts/Other/InterfaceTypeMultiAttribute.cs
+++ Assets/Scripts/Other/InterfaceTypeMultiAttribute.cs
@@ -22,60 +22,59 @@ public class InterfaceTypeMultiAttribute : MultiPropertyAttribute
 #if UNITY_EDITOR
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
-        if (property.propertyType != SerializedPropertyType.ObjectReference)
-        {
-            EditorGUI.LabelField(position, label.text, "InterfaceType Attribute can only be used with MonoBehaviour Components!");
-            return;
-        }
-
-        // Pick a specific component
-        MonoBehaviour oldComp = property.objectReferenceValue as MonoBehaviour;
-
-        GameObject temp = null;
-        string oldName = "";
-
-        if (Event.current.type == EventType.Repaint)
-        {
-            if (oldComp == null)
+            if (property.propertyType != SerializedPropertyType.ObjectReference)
             {
-                temp = new GameObject("None [" + type.Name + "]");
-                oldComp = temp.AddComponent<MonoInterfaceMulti>();
+                EditorGUI.LabelField(position, label.text, "InterfaceType Attribute can only be used with MonoBehaviour Components!");
+                return;
             }
-            else
+
+            // Pick a specific component
+            MonoBehaviour oldComp = property.objectReferenceValue as MonoBehaviour;
+
+            GameObject temp = null;
+            string oldName = "";
+
+            if (Event.current.type == EventType.Repaint)
             {
-                oldName = oldComp.name;
-                oldComp.name = oldName + " [" + type.Name + "]";
+                if (oldComp == null)
+                {
+                    temp = new GameObject("None [" + type.Name + "]");
+                    oldComp = temp.AddComponent<MonoInterfaceMulti>();
+                }
+                else
+                {
+                    oldName = oldComp.name;
+                    oldComp.name = oldName + " [" + type.Name + "]";
+                }
             }
-        }
 
-        MonoBehaviour comp = EditorGUI.ObjectField(position, label, oldComp, typeof(MonoBehaviour), true) as MonoBehaviour;
+            MonoBehaviour comp = EditorGUI.ObjectField(position, label, oldComp, typeof(MonoBehaviour), true) as MonoBehaviour;
 
-        if (Event.current.type == EventType.Repaint)
-        {
-            if (temp != null)
-                GameObject.DestroyImmediate(temp);
-            else
-                oldComp.name = oldName;
-        }
+            if (Event.current.type == EventType.Repaint)
+            {
+                if (temp != null)
+                    GameObject.DestroyImmediate(temp);
+                else
+                    oldComp.name = oldName;
+            }
 
-        // Make sure something changed.
-        if (oldComp == comp) return;
+            // Make sure something changed.
+            if (oldComp == comp) return;
 
-        // If a component is assigned, make sure it is the interface we are looking for.
-        if (comp != null)
-        {
-            // Make sure component is of the right interface
-            if (comp.GetType() != type)
-                // Component failed. Check game object.
-                comp = comp.gameObject.GetComponent(type) as MonoBehaviour;
+            // If a component is assigned, make sure it is the interface we are looking for.
+            if (comp != null)
+            {
+                // Make sure component is of the right interface
+                if (comp.GetType() != type)
+                    // Component failed. Check game object.
+                    comp = comp.gameObject.GetComponent(type) as MonoBehaviour;
 
-            // Item failed test. Do not override old component
-            if (comp == null) return;
-        }
+                // Item failed test. Do not override old component
+                if (comp == null) return;
+            }
 
-        property.objectReferenceValue = comp;
-        property.serializedObject.ApplyModifiedProperties();
-
+            property.objectReferenceValue = comp;
+            property.serializedObject.ApplyModifiedProperties();
     }
 #endif
 
