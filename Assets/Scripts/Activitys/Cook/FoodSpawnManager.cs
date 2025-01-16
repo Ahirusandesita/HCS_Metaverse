cc3a762b72fa4e701ee73945ae26fa22aa4d9a05
STD\s20227002 <shironeko248@gmail.com>
2025-01-16 11:57:47 +0900

    ふぉおｄ２

--- Assets/Scripts/Activitys/Cook/FoodSpawnManager.cs
+++ Assets/Scripts/Activitys/Cook/FoodSpawnManager.cs
@@ -225,8 +225,8 @@ public class FoodSpawnManager : MonoBehaviour, ISelectedNotification
             }
         }
         networkInformations.Remove(information);
-        GateOfFusion.Instance.Despawn(information.NetworkView);
         Destroy(information.NetworkView.LocalView.gameObject);
+        GateOfFusion.Instance.Despawn(information.NetworkView);
     }
 }
 
