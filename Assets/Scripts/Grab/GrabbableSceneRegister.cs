170ccfa3de61b9ee703990e07635d340d97a7bd3
STD\s20227005 <Ahirusandesita>
2025-01-27 14:33:26 +0900

    no message

---
+++ Assets/Scripts/Grab/GrabbableSceneRegister.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GrabbableSceneRegister : MonoBehaviour, IGrabbableActiveChangeRequester
+{
+    [SerializeField]
+    private List<RegisterSceneInInspector> registerSceneInInspectors = new List<RegisterSceneInInspector>();
+
+    void Start()
+    {
+        foreach (RegisterSceneInInspector item in registerSceneInInspectors)
+        {
+            if (item == UnityEngine.SceneManagement.SceneManager.GetActiveScene().name)
+            {
+                return;
+            }
+        }
+        this.GetComponent<ISwitchableGrabbableActive>().Regist(this);
+        this.GetComponent<ISwitchableGrabbableActive>().Inactive(this);
+    }
+}
