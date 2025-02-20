88c2a6f797b7738f814ee490a23addbeebd11e0b
STD\s20227005 <Ahirusandesita>
2025-02-20 09:51:02 +0900

    no message

--- Assets/Scripts/Login/LoginButton.cs
+++ Assets/Scripts/Login/LoginButton.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Cysharp.Threading.Tasks;
 
-public class LoginButton : MonoBehaviour,IPointerClickRegistrable
+public class LoginButton : MonoBehaviour, IPointerClickHandler
 {
-	[SerializeField]
-	private LoginManager _loginManager;
-	private void Reset()
-	{
-		_loginManager = FindAnyObjectByType<LoginManager>();
-	}
-	public void OnPointerClick(PointerEventData data)
-	{
-		_loginManager.ExecuteLogin().Forget();
-	}
+    [SerializeField]
+    private LoginManager _loginManager;
+    private void Reset()
+    {
+        _loginManager = FindAnyObjectByType<LoginManager>();
+    }
+    public void OnPointerClick(PointerEventData data)
+    {
+        _loginManager.ExecuteLogin().Forget();
+    }
 }
