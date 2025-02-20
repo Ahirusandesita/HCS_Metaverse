c8700f5fab5b38b3ead3eb6325a744df52349a38
STD\s20225010 <wata.kuma@hotmail.co.jp>
2025-02-20 12:05:22 +0900

    Shop戻れるようになった

--- Assets/Scripts/Canvas/ActivityStart/ReturnMainCityButton.cs
+++ Assets/Scripts/Canvas/ActivityStart/ReturnMainCityButton.cs
@@ -4,21 +4,27 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using Cysharp.Threading.Tasks;
 
-public class ReturnMainCityButton : MonoBehaviour,IPointerClickHandler
+public class ReturnMainCityButton : MonoBehaviour, IPointerClickHandler
 {
 	[SerializeField]
 	private MyRoomLoader _myRoomLoader;
 
 	public async void OnPointerClick(PointerEventData data)
 	{
-		await _myRoomLoader.UnLoad();
+		if (_myRoomLoader != null)
+		{
+			await _myRoomLoader.UnLoad();
+		}
 		GateOfFusion.Instance.ReturnMainCity();
 	}
 
 	[ContextMenu("Click")]
 	private async UniTaskVoid Test()
 	{
-		await _myRoomLoader.UnLoad();
+		if (_myRoomLoader != null)
+		{
+			await _myRoomLoader.UnLoad();
+		}
 		GateOfFusion.Instance.ReturnMainCity();
 	}
 }
